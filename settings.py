045b64cb500419ba641c48a0cbd23cfc69c4f1b5
Wolf_Stievano <79229960+wolf-stievano@users.noreply.github.com>
2023-05-17 18:58:27 -0300

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-import configparser
-import os
-from api.db import get_db_collections
-
-config = configparser.ConfigParser()
-config.read(os.path.abspath(os.path.join(".ini")))
-
-MONGO_URI = config['PROD']['DB_URI']
-tasks_collection, users_collection = get_db_collections(MONGO_URI)
